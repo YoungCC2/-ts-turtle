116f21d21c98b598be82b1d03786dce211072617
yanghang <yanghang@xyd.cn>
2020-04-27 18:20:52 +0800

    1.0

--- function/function.js
+++ function/function.js
@@ -1,9 +1,28 @@
-Function.prototype.mybind = function (context,...args){
+Function.prototype.mybind = function (context, ...args) {
     let fun = this;
     function bound(...arg2) {
         let self = this instanceof bound ? this : context
 
-        
+        return fun.apply(self, args.concat(...arg2))
     }
 
+    bound.prototype = Object.create(fun.prototype)
+
+    return bound;
 }
+
+Function.prototype.mycall = function (context,...args) {
+    context.fun = this;
+    return context.fun(...args);
+}
+
+
+function foo (){
+    console.log(this)
+}
+
+var obj = {
+    x: "xx"
+}
+
+console.log(foo.mycall(obj))
